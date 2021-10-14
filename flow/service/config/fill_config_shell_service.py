d27e4478cdf208fc0226154a6dc6740620ffecfe
wingo.wen <wingo.wen@welab-inc.com>
2021-10-14 11:50:44 +0800

    global config internet auto fill

--- flow/service/config/fill_config_shell_service.py
+++ flow/service/config/fill_config_shell_service.py
@@ -11,35 +11,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from common.python.common import consts
 from common.python.db.global_config_dao import GlobalConfigDao
 from common.python.utils.conf_utils import get_env_config
+from common.python.utils.log_utils import LoggerFactory
 
 ENV_INTRANET_IP = get_env_config(consts.ENV_CONF_KEY_INTRANET_IP) or 'ip'
 ENV_GATEWAY_PORT = get_env_config(consts.ENV_CONF_KEY_GATEWAY_PORT) or 'port'
 ENV_FLOW_PORT = get_env_config(consts.ENV_CONF_KEY_FLOW_PORT) or 'port'
 ENV_NGINX_PORT = get_env_config(consts.ENV_CONF_KEY_NGINX_PORT) or 'port'
 
-
-class GlobalSettingInit:
-    """
-    Initialize the relevant configuration based on the environment variables
-    """
-
-    @staticmethod
-    def is_ready_to_boot():
-        """
-        The Member must to be initialized
-        Check the variable global-config.member_id
-        :return: bool
-        """
-        member_info = GlobalConfigDao.getMemberInfo()
-        gateway_config = GlobalConfigDao.getGatewayConfig()
-        if (member_info.member_id is None) or (gateway_config.intranet_base_uri is None):
-            return False
-        else:
-            return True
-
+logger = LoggerFactory.get_logger("InitConfig")
 
 if __name__ == '__main__':
-    print(GlobalSettingInit().is_ready_to_boot())
+    """
+    Initialize the configuration of the system based on the profile
+
+    wefe_board.intranet_base_uri
+,   wefe_flow.intranet_base_uri
+    wefe_gateway.intranet_base_uri
+    wefe_serving.intranet_base_uri
+    """
+    logger.info('Init Config')
+    intranet_dict = {
+        'wefe_board': 'http://' + ENV_INTRANET_IP + ':' + ENV_NGINX_PORT + '/board-service',
+        'wefe_flow': 'http://' + ENV_INTRANET_IP + ':' + ENV_FLOW_PORT,
+        'wefe_gateway': ENV_INTRANET_IP + ':' + ENV_GATEWAY_PORT,
+        'wefe_serving': ''
+    }
+    GlobalConfigDao.fill_intranet_base_uri(intranet_dict)
+    logger.info('Init Success')
