164306e5e888f2fa41ac5dadcd2edd1831d77c3a
zane.luo <zane.luo@welab-inc.com>
2021-10-29 11:26:18 +0800

    完成 api：global_config/config_properties；新增函数计算费用上限配置项。

--- common/java/common-web/src/main/java/com/welab/wefe/common/web/api/base/AbstractApi.java
+++ common/java/common-web/src/main/java/com/welab/wefe/common/web/api/base/AbstractApi.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2021 Tianmian Tech. All Rights Reserved.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -197,6 +197,10 @@ public abstract class AbstractApi<In extends AbstractApiInput, Out> {
         result.message = e.getMessage();
         result.data = data;
         return result;
+    }
+
+    protected ApiResult<Out> fail(Exception e) {
+        return fail(-1, e.getClass().getSimpleName() + " " + e.getMessage(), null);
     }
 
     protected ApiResult<Out> fail(String message) {
