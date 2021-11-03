51df0796b6ed50d71f68c4ca5c80a195db92074d
yuxin.zhang <abc~!@123>
2021-11-03 18:12:28 +0800

    修改bug

--- board/board-service/src/main/java/com/welab/wefe/board/service/api/union/MemberRealNameAuthInfoQueryApi.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/api/union/MemberRealNameAuthInfoQueryApi.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.welab.wefe.board.service.sdk.UnionService;
 import com.welab.wefe.common.exception.StatusCodeWithException;
 import com.welab.wefe.common.web.api.base.AbstractApi;
+import com.welab.wefe.common.web.api.base.Api;
 import com.welab.wefe.common.web.dto.AbstractApiInput;
 import com.welab.wefe.common.web.dto.ApiResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import java.io.IOException;
  * @author: yuxin.zhang
  * @date: 2021/11/3
  */
+@Api(path = "union/member/realname/authInfo/query", name = "apply realname auth")
 public class MemberRealNameAuthInfoQueryApi extends AbstractApi<AbstractApiInput, JSONObject> {
     @Autowired
     private UnionService unionService;
