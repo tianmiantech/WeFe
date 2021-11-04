f60abe37b535ce6e4e239fc9bf026e80d563934a
yuxin.zhang <abc~!@123>
2021-11-04 11:08:11 +0800

    修改board 实名认证文件上传接口bug

--- board/board-service/src/main/java/com/welab/wefe/board/service/sdk/UnionService.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/sdk/UnionService.java
@@ -427,7 +427,7 @@ public class UnionService extends AbstractService {
                             contentType,
                             file.getOriginalFilename()
                     );
-                    request.appendParameter(item.getKey(), streamBody);
+                    request.appendParameter(item.getKey(), item.getValue());
                 } catch (IOException e) {
                     StatusCode.FILE_IO_ERROR.throwException(e);
                 }
