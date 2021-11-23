56a04a45da069a4619092db0a9c50864e223068d
yuxin.zhang <abc~!@123>
2021-11-23 10:44:44 +0800

    优化manager mongo初始化方案

--- manager/manager-service/src/main/java/com/welab/wefe/manager/service/ManagerService.java
+++ manager/manager-service/src/main/java/com/welab/wefe/manager/service/ManagerService.java
@@ -1,7 +1,6 @@
 package com.welab.wefe.manager.service;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
-import com.welab.wefe.common.util.Md5;
 import com.welab.wefe.common.web.CurrentAccount;
 import com.welab.wefe.common.web.Launcher;
 import com.welab.wefe.common.web.config.ApiBeanNameGenerator;
@@ -10,10 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -23,7 +22,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         DataSourceAutoConfiguration.class,
         DruidDataSourceAutoConfigure.class,
         MongoAutoConfiguration.class,
-        MongoDataAutoConfiguration.class})
+        MongoDataAutoConfiguration.class,
+        TransactionAutoConfiguration.class
+})
 @EnableScheduling
 @ComponentScan(
         basePackages = {"com.welab.wefe.common.data.mongodb"},
