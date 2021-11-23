bb45de6139604634ed685ac27443e8c2e711ca99
winter.zou <winter.zou@welab-inc.com>
2021-11-23 15:21:18 +0800

    纵向流程必须先对齐

--- board/board-service/src/main/java/com/welab/wefe/board/service/component/feature/BinningComponent.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/component/feature/BinningComponent.java
@@ -54,6 +54,11 @@ public class BinningComponent extends AbstractComponent<BinningComponent.Params>
     @Override
     protected void checkBeforeBuildTask(FlowGraph graph, List<TaskMySqlModel> preTasks, FlowGraphNode node, Params params) throws FlowNodeException {
 
+        FlowGraphNode intersectionNode = graph.findOneNodeFromParent(node, ComponentType.Intersection);
+        if (intersectionNode == null) {
+            throw new FlowNodeException(node, "请在前面添加样本对齐组件。");
+        }
+        
         if (CollectionUtils.isEmpty(params.getMembers())) {
             throw new FlowNodeException(node, "请添加分箱策略");
         }
--- board/board-service/src/main/java/com/welab/wefe/board/service/component/feature/VertPCAComponent.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/component/feature/VertPCAComponent.java
@@ -46,7 +46,10 @@ public class VertPCAComponent extends AbstractComponent<VertPCAComponent.Params>
     @Override
     protected void checkBeforeBuildTask(FlowGraph graph, List<TaskMySqlModel> preTasks, FlowGraphNode node,
                                         Params params) throws FlowNodeException {
-
+        FlowGraphNode intersectionNode = graph.findOneNodeFromParent(node, ComponentType.Intersection);
+        if (intersectionNode == null) {
+            throw new FlowNodeException(node, "请在前面添加样本对齐组件。");
+        }
     }
 
     @Override
