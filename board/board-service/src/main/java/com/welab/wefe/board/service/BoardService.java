0cae8de4bbddd2ca03d7bb182ecbfa28e5218c84
zane.luo <zane.luo@welab-inc.com>
2021-12-13 17:08:01 +0800

    增加 OnlineDemoScheduledService 用于自动清理 Demo 环境中的嘈杂数据

--- board/board-service/src/main/java/com/welab/wefe/board/service/BoardService.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/BoardService.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2021 Tianmian Tech. All Rights Reserved.
- * 
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -48,13 +48,13 @@ import java.nio.charset.StandardCharsets;
  * @author hunter.zhao
  */
 @SpringBootApplication
-@EnableScheduling
 @EnableAsync
 @ComponentScan(
         lazyInit = true,
         nameGenerator = ApiBeanNameGenerator.class,
         basePackageClasses = {BoardService.class, Launcher.class, StorageManager.class}
 )
+@EnableScheduling
 public class BoardService implements ApplicationContextAware {
 
     private static final Logger LOG = LoggerFactory.getLogger(BoardService.class);
---
+++ board/board-service/src/main/java/com/welab/wefe/board/service/listener/README.md
@@ -0,0 +1,8 @@
+SpringBoot 启动时所有事件及其顺序：
+
+* ApplicationStartingEvent
+* ApplicationEnvironmentPreparedEvent
+* ApplicationPreparedEvent
+* ApplicationStartedEvent
+* ApplicationReadyEvent
+* ApplicationFailedEvent
