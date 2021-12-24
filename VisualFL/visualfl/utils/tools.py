06b9dfef82ddc2deb586661decfa5d6cc848dba2
tracy.zhang <tracy.zhang@welab-inc.com>
2021-12-24 14:20:57 +0800

    remove get_data_to_db method

--- VisualFL/visualfl/utils/tools.py
+++ VisualFL/visualfl/utils/tools.py
@@ -1,8 +1,6 @@
-import os
 import aiohttp
 import asyncio
 import json
-from visualdl import LogReader
 from visualfl.db.task_dao import TaskDao
 import time
 import logging
@@ -19,29 +17,6 @@ def post(url, json_data):
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(post_co())
-
-def get_last_file(data_dir):
-    list = os.listdir(data_dir)
-    list.sort(key=lambda fn:os.path.getmtime(data_dir+'/'+fn))
-    filepath = os.path.join(data_dir,list[-1])
-
-    return filepath
-
-def get_data_to_db(task_id,log_dir,tag,component_name):
-    file_path = get_last_file(log_dir)
-    reader = LogReader(file_path=file_path)
-    tags = reader.get_tags()
-    losslist = reader.get_data('scalar', 'accuracy_0.tmp_0')
-
-    if len(losslist)>0:
-        metric_result,train_loss,data = {},{},{}
-        for loss in losslist:
-            data[loss.id] = dict(value=loss.value, timestamp=loss.timestamp)
-        train_loss.update(metric_name=tag)
-        train_loss.update(data=data)
-        metric_result.update(train_loss=train_loss)
-
-        TaskDao(task_id).save_task_result(task_result=metric_result,component_name=component_name,type=tag)
 
 def save_data_to_db(task_id,tag,value,step,component_name):
     try:
