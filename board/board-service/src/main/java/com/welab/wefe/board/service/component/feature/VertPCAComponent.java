8329e65d944fa89d2879530b44995dad9e0a658d
winter.zou <winter.zou@welab-inc.com>
2021-12-28 16:54:01 +0800

    update

--- board/board-service/src/main/java/com/welab/wefe/board/service/component/feature/VertPCAComponent.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/component/feature/VertPCAComponent.java
@@ -28,31 +28,27 @@ import com.welab.wefe.board.service.component.base.io.IODataType;
 import com.welab.wefe.board.service.component.base.io.InputMatcher;
 import com.welab.wefe.board.service.component.base.io.Names;
 import com.welab.wefe.board.service.component.base.io.OutputItem;
-import com.welab.wefe.board.service.component.feature.VertPCAComponent.Params.MemberInfoModel;
 import com.welab.wefe.board.service.database.entity.job.TaskMySqlModel;
 import com.welab.wefe.board.service.database.entity.job.TaskResultMySqlModel;
-import com.welab.wefe.board.service.dto.entity.MemberModel;
 import com.welab.wefe.board.service.exception.FlowNodeException;
 import com.welab.wefe.board.service.model.FlowGraph;
 import com.welab.wefe.board.service.model.FlowGraphNode;
 import com.welab.wefe.board.service.service.CacheObjects;
 import com.welab.wefe.common.enums.ComponentType;
 import com.welab.wefe.common.enums.TaskResultType;
-import com.welab.wefe.common.fieldvalidate.annotation.Check;
 import com.welab.wefe.common.util.JObject;
-import com.welab.wefe.common.web.dto.AbstractApiInput;
 
 @Service
-public class VertPCAComponent extends AbstractComponent<VertPCAComponent.Params> {
+public class VertPCAComponent extends AbstractComponent<VertOneHotComponent.Params> {
     @Override
     protected void checkBeforeBuildTask(FlowGraph graph, List<TaskMySqlModel> preTasks, FlowGraphNode node,
-                                        Params params) throws FlowNodeException {
+    		VertOneHotComponent.Params params) throws FlowNodeException {
         FlowGraphNode intersectionNode = graph.findOneNodeFromParent(node, ComponentType.Intersection);
         if (intersectionNode == null) {
             throw new FlowNodeException(node, "请在前面添加样本对齐组件。");
         }
         
-        List<MemberInfoModel> members = params.getMembers();
+        List<VertOneHotComponent.Params.MemberInfoModel> members = params.getMembers();
         if (members.size() > 2) {
             throw new FlowNodeException(node, "两方纵向PCA参与成员不能超过两方。");
         }
@@ -60,7 +56,7 @@ public class VertPCAComponent extends AbstractComponent<VertPCAComponent.Params>
 
     @Override
     protected JSONObject createTaskParams(FlowGraph graph, List<TaskMySqlModel> preTasks, FlowGraphNode node,
-                                          Params params) throws FlowNodeException {
+    		VertOneHotComponent.Params params) throws FlowNodeException {
 
         JSONObject taskParam = new JSONObject();
 
@@ -103,39 +99,5 @@ public class VertPCAComponent extends AbstractComponent<VertPCAComponent.Params>
     @Override
     public List<OutputItem> outputs(FlowGraph graph, FlowGraphNode node) throws FlowNodeException {
         return Arrays.asList(OutputItem.of(Names.JSON_RESULT, IODataType.Json));
-    }
-
-    public static class Params extends AbstractApiInput {
-
-        @Check(name = "成员信息", require = true)
-        private List<MemberInfoModel> members;
-
-        private boolean crossParties;
-
-        public List<MemberInfoModel> getMembers() {
-            return members;
-        }
-
-        public void setMembers(List<MemberInfoModel> members) {
-            this.members = members;
-        }
-
-        public boolean getCrossParties() {
-            return crossParties;
-        }
-
-        public static class MemberInfoModel extends MemberModel {
-            @Check(name = "特征列", require = true)
-            private List<String> features = new ArrayList<>();
-
-            public List<String> getFeatures() {
-                return features;
-            }
-
-            public void setFeatures(List<String> features) {
-                this.features = features;
-            }
-
-        }
     }
 }
