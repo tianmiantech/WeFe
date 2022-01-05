7b38e7f73ccf8c5a0cebff3e1ec19bda5369fce4
eval.tan <eval.tan@welab-inc.com>
2022-01-05 13:38:40 +0800

    mpc package script

---
+++ mpc/package/build.sh
@@ -0,0 +1,11 @@
+# bin/bash
+cd ..
+rm -rf libs/
+mkdir libs
+mvn clean package -Dmaven.test.skip=true
+
+cp mpc-pir/mpc-pir-server/target/mpc-pir-server-1.0.0-shaded.jar libs/mpc-pir-server-1.0.0.jar
+cp mpc-pir/mpc-pir-sdk/target/mpc-pir-sdk-1.0.0-shaded.jar libs/mpc-pir-sdk-1.0.0.jar
+cp mpc-psi/mpc-psi-sdk/target/mpc-psi-sdk-1.0.0-shaded.jar libs/mpc-psi-sdk-1.0.0.jar
+cp mpc-sa/mpc-sa-server/target/mpc-sa-server-1.0.0-shaded.jar libs/mpc-sa-server-1.0.0.jar
+cp mpc-sa/mpc-sa-sdk/target/mpc-sa-sdk-1.0.0-shaded.jar libs/mpc-sa-sdk-1.0.0.jar
