9557f5de0de30e742edda464387f064abd7fb855
wingo.wen <wingo.wen@welab-inc.com>
2022-01-07 16:34:24 +0800

    fix: use nvm to control node and npm version

--- jenkins_docker_deploy.sh
+++ jenkins_docker_deploy.sh
@@ -17,7 +17,7 @@ export DEPLOY_MODE=$3
 export BASE_DIR=/data/jenkins_docker_deploy
 export SHELL_DIR=$WORKSPACE'/release/docker/deploy_shell'
 # oss 上传工具地址
-export OSS_TOOL=/data/aliyun_oss/oss
+export OSS_DIR=/data/aliyun_oss
 # FC 环境部署的工作空间
 export FC_WORKSPACE=/data/jenkins_workspace/workspace/wefe_fc_nas
 
@@ -69,7 +69,8 @@ if [[ $DEPLOY_MODE == 'pack' ]]; then
   echo '====== ORIGIN DEPLOY MODE ======'
   cd $WORKSPACE
   tar -cvf deploy_package_resource.tar deploy_package_resource > /dev/null
-  ./$OSS_TOOL cp ./deploy_package_resource.tar oss://welab-wefe-release/
+  cd $OSS_DIR
+  ./oss cp ./deploy_package_resource.tar oss://welab-wefe-release/
   echo '====== DONE ======'
   echo '====== DOWNLOAD LINK https://welab-wefe-release.oss-cn-shenzhen.aliyuncs.com/deploy_package_resource.tar ======'
   exit 1
