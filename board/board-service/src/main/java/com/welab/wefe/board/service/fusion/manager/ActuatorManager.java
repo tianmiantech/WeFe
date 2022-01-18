0123d89f63a5a0c81452a964903532768d9385a7
hunter.zhao <7GHMDBCENU5H>
2022-01-18 19:02:42 +0800

    wefe-fusion审核任务

--- board/board-service/src/main/java/com/welab/wefe/board/service/fusion/manager/ActuatorManager.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/fusion/manager/ActuatorManager.java
@@ -104,7 +104,7 @@ public class ActuatorManager {
                     .append("spend", actuator.getSpend())
                     .append("status", "Running")
                     .append("estimated_spend", actuator.getEstimatedSpend())
-                    .append("progress", actuator.progress());
+                    .append("progress", actuator.progress() * 100.0);
         }
 
         FusionTaskMySqlModel model = fusionTaskService.findByBusinessId(businessId);
@@ -117,7 +117,7 @@ public class ActuatorManager {
                     .append("data_count", model.getDataCount())
                     .append("spend", model.getSpend())
                     .append("status", model.getStatus())
-                    .append("progress", model.getProcessedCount().doubleValue() / model.getDataCount().doubleValue());
+                    .append("progress", (model.getProcessedCount().doubleValue() / model.getDataCount().doubleValue()) * 100.0);
         }
 
         return null;
