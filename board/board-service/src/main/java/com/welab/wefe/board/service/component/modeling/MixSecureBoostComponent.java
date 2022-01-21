4a0eeaefab48b964129c55dac73eda2642ad4684
winter.zou <winter.zou@welab-inc.com>
2022-01-21 17:03:03 +0800

    add args

--- board/board-service/src/main/java/com/welab/wefe/board/service/component/modeling/MixSecureBoostComponent.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/component/modeling/MixSecureBoostComponent.java
@@ -188,6 +188,13 @@ public class MixSecureBoostComponent extends AbstractModelingComponent<MixSecure
             private int earlyStoppingRounds;
             @Check(name = "工作模式", require = true)
             private String workMode; // normal、layered、skip
+            // 当work_mode==layered时，需要下面三个参数
+            @Check(name = "总层数")
+            private int maxDepth;
+            @Check(name = "promoter层数")
+            private int promoterDepth;
+            @Check(name = "provider层数")
+            private int providerDepth;
 
             public String getTaskType() {
                 return taskType;
@@ -267,6 +274,30 @@ public class MixSecureBoostComponent extends AbstractModelingComponent<MixSecure
 
 			public void setWorkMode(String workMode) {
 				this.workMode = workMode;
+			}
+			
+			public int getMaxDepth() {
+				return maxDepth;
+			}
+
+			public void setMaxDepth(int maxDepth) {
+				this.maxDepth = maxDepth;
+			}
+
+			public int getPromoterDepth() {
+				return promoterDepth;
+			}
+
+			public void setPromoterDepth(int promoterDepth) {
+				this.promoterDepth = promoterDepth;
+			}
+
+			public int getProviderDepth() {
+				return providerDepth;
+			}
+
+			public void setProviderDepth(int providerDepth) {
+				this.providerDepth = providerDepth;
 			}
         }
 
