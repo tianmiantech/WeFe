14550be4ae3b8b081028d18a6e910bc096c0f42a
jensen.liang <jensen.liang@welab-inc.com>
2022-02-10 17:55:14 +0800

    调整nas init执行步骤

--- common/python/calculation/fc/function/wefe-fc/fc_deploy.sh
+++ common/python/calculation/fc/function/wefe-fc/fc_deploy.sh
@@ -19,9 +19,6 @@ nas_upload(){
 
   s config add --AccessKeyID $access_key_id --AccessKeySecret  $access_key_secret --AccountID $account_id --aliasName s-config
 
-  # init project
-  s nas init
-
   # copy root, python, config.properties
   has_python=`s nas command ls  -l nas:///mnt/auto`
   if [[ $has_python =~ 'python' ]]
@@ -32,8 +29,13 @@ nas_upload(){
     if [ ! -d $root_dir ]; then
       echo "local dir has no python, root environment, now run 's build --use-docker' command to download ..."
       s build --use-docker --debug
+      # init project
+      s nas init
     else
       echo "remote nas has no python environment, now upload to nas ..."
+
+      # init project
+      s nas init
       s nas upload /data/environment/.s/python nas:///mnt/auto/ -r -o --debug
     fi
 
