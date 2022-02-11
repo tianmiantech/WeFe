304a25745170e34ba9f843fe07f283bd394ffd34
ivenn.zheng <ivenn.zheng@welabb-inc.com>
2022-02-11 17:39:46 +0800

    修改按钮跳转 调整

--- serving/serving-website/src/views/client/client-edit.vue
+++ serving/serving-website/src/views/client/client-edit.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <el-card class="page" shadow="never">
+    <el-card v-loading="loading" class="page" shadow="never">
 
         <h2 class="title">编辑客户</h2>
 
@@ -99,6 +99,7 @@ export default {
         };
 
         return {
+            loading: false,
             client: {
                 id: '',
                 name: '',
@@ -138,13 +139,15 @@ export default {
     computed: {
         ...mapGetters(['userInfo']),
     },
-    created() {
+    async created() {
+
+        this.loading = true
         if (this.$route.query.id) {
             this.clientId = this.$route.query.id
-            this.getClientById(this.$route.query.id)
+            await this.getClientById(this.$route.query.id)
         }
         this.client.status = this.$route.query.status
-
+        this.loading = false
     },
     methods: {
 
