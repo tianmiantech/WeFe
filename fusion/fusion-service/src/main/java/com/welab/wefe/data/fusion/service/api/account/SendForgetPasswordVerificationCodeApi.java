4fad52a56217c1e37aeccdb91760bf83e9c58df0
hunter.zhao <7GHMDBCENU5H>
2022-02-21 16:02:40 +0800

    wefe-fusion旧版融合忘记密码

---
+++ fusion/fusion-service/src/main/java/com/welab/wefe/data/fusion/service/api/account/SendForgetPasswordVerificationCodeApi.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2021 Tianmian Tech. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.welab.wefe.data.fusion.service.api.account;
+
+import com.welab.wefe.common.exception.StatusCodeWithException;
+import com.welab.wefe.common.fieldvalidate.annotation.Check;
+import com.welab.wefe.common.web.api.base.AbstractApi;
+import com.welab.wefe.common.web.api.base.Api;
+import com.welab.wefe.common.web.dto.AbstractApiInput;
+import com.welab.wefe.common.web.dto.ApiResult;
+import com.welab.wefe.common.web.dto.NoneApiOutput;
+import com.welab.wefe.common.wefe.enums.VerificationCodeBusinessType;
+import com.welab.wefe.data.fusion.service.service.verificationcode.VerificationCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+
+/**
+ * Send forget password verification code
+ *
+ * <p>
+ *     Due to problems left over by history, the interface for sending verification code is temporarily placed here
+ * </p>
+ *
+ * @author aaron.li
+ * @date 2021/11/11 09:45
+ **/
+@Api(path = "union/send_forget_password_sms_code", name = "send sms verification code", login = false)
+public class SendForgetPasswordVerificationCodeApi extends AbstractApi<SendForgetPasswordVerificationCodeApi.Input, NoneApiOutput> {
+
+    @Autowired
+    private VerificationCodeService verificationCodeService;
+
+    @Override
+    protected ApiResult<NoneApiOutput> handle(Input input) throws StatusCodeWithException, IOException {
+        verificationCodeService.send(input.phoneNumber, VerificationCodeBusinessType.accountForgetPassword);
+        return success();
+    }
+
+    public static class Input extends AbstractApiInput {
+        @Check(require = true)
+        private String phoneNumber;
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}
---
+++ fusion/fusion-service/src/main/java/com/welab/wefe/data/fusion/service/service/verificationcode/AliyunSmsClient.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2021 Tianmian Tech. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.welab.wefe.data.fusion.service.service.verificationcode;
+
+import com.aliyun.dysmsapi20170525.Client;
+import com.aliyun.dysmsapi20170525.models.SendSmsRequest;
+import com.aliyun.dysmsapi20170525.models.SendSmsResponse;
+import com.aliyun.teaopenapi.models.Config;
+import com.welab.wefe.common.util.JObject;
+import com.welab.wefe.common.verification.code.AbstractClient;
+import com.welab.wefe.common.verification.code.AbstractResponse;
+
+import java.util.Map;
+
+/**
+ * Aliyun sms client
+ *
+ * @author aaron.li
+ * @date 2022/1/19 10:48
+ **/
+public class AliyunSmsClient extends AbstractClient {
+    private Client client;
+
+    public AliyunSmsClient(Map<String, Object> extendParams) {
+        super(extendParams);
+    }
+
+    public static AliyunSmsClient createClient(String accessKeyId, String accessKeySecret, Map<String, Object> extendParams) throws Exception {
+        Config config = new Config().setAccessKeyId(accessKeyId).setAccessKeySecret(accessKeySecret);
+        config.endpoint = "dysmsapi.aliyuncs.com";
+        AliyunSmsClient aliyunSmsClient = new AliyunSmsClient(extendParams);
+        aliyunSmsClient.client = new Client(config);
+        return aliyunSmsClient;
+    }
+
+    @Override
+    public AbstractResponse send(String mobile, String verificationCode) throws Exception {
+        SendSmsRequest sendSmsRequest = new SendSmsRequest();
+        sendSmsRequest.setPhoneNumbers(mobile);
+        Map<String, Object> extendParams = getExtendParams();
+        sendSmsRequest.setSignName(String.valueOf(extendParams.get("SignName")));
+        sendSmsRequest.setTemplateCode(String.valueOf(extendParams.get("templateCode")));
+        sendSmsRequest.setTemplateParam(JObject.create("code", verificationCode).toString());
+        SendSmsResponse sendSmsResponse = this.client.sendSms(sendSmsRequest);
+        return new AliyunSmsResponse(sendSmsResponse);
+    }
+}
