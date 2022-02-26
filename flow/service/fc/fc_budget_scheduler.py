034c42fbe3a09bd18acb53bf999f73344ace5dcc
ivenn.zheng <ivenn.zheng@welabb-inc.com>
2022-02-26 12:06:37 +0800

    修改函数限额暂停任务逻辑

--- flow/service/fc/fc_budget_scheduler.py
+++ flow/service/fc/fc_budget_scheduler.py
@@ -25,6 +25,7 @@ import errno
 import os
 import json
 
+
 class FcBudgetScheduler(threading.Thread):
     """
         Detects whether the budget of function computing exceeds the limit,
@@ -58,8 +59,7 @@ class FcBudgetScheduler(threading.Thread):
             self.logger.warn("函数计算已超最大日限额, 随即停止所有任务！")
         with DB.connection_context():
             for task in task_list:
-                self.logger.info(f'task config:{json.loads(task.task_conf)}')
-                if json.loads(task.task_conf)['env']['backend'] == 'FC':
+                if json.loads(task.task_conf)['job']['env']['backend'] == 'FC':
                     killed = self.kill_task(task)
                     if killed:
                         self.logger.info(f"kill task {task.name}({task.task_id}) process pid: {task.pid} success!")
@@ -150,3 +150,4 @@ class FcBudgetScheduler(threading.Thread):
                 schedule_logger().exception("函数计算预算检测出现异常：%s", e)
                 time.sleep(5)
                 continue
+
