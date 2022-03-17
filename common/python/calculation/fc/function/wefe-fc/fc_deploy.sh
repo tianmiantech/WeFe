7554e7df9f957603b7d0aba7f86dbd9227ed2660
ivenn.zheng <ivenn.zheng@welabb-inc.com>
2022-03-17 16:23:36 +0800

    修改函数部署脚本

--- common/python/calculation/fc/function/wefe-fc/fc_deploy.sh
+++ common/python/calculation/fc/function/wefe-fc/fc_deploy.sh
@@ -24,10 +24,10 @@ nas_upload(){
   has_env=`s nas command ls  -l nas:///mnt/auto`
   if [[ $has_env == $nas_env ]]
   then
-    echo 'has env dir, upload env dir'
+    echo 'has env dir, upload pythonCode ...'
     s nas upload -r -o ./$nas_env/pythonCode /mnt/auto/$nas_env/ --debug
   else
-    echo 'has not env dir, upload pythonCode ...'
+    echo 'has not env dir, upload env dir ...'
     s nas upload -r -o ./$nas_env /mnt/auto/ --debug
   fi
   rm -rf $nas_env
