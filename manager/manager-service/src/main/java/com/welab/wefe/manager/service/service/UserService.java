36c61c4c116ab8ad03ae5336ea7426006b07492b
yuxin.zhang <yuxin.zhang@wolaidai.com>
2022-03-18 13:04:20 +0800

    修改bug

--- manager/manager-service/src/main/java/com/welab/wefe/manager/service/service/UserService.java
+++ manager/manager-service/src/main/java/com/welab/wefe/manager/service/service/UserService.java
@@ -98,7 +98,7 @@ public class UserService extends AbstractAccountService {
             throw new StatusCodeWithException("不能重置超级管理员密码", StatusCode.PERMISSION_DENIED);
         }
 
-        if(!user.getPassword().equals(hashPasswordWithSalt(adminPassword,user.getSalt()))){
+        if(!CurrentAccount.get().getPassword().equals(hashPasswordWithSalt(adminPassword, CurrentAccount.get().getSalt()))){
             throw new StatusCodeWithException("管理员密码错误", StatusCode.PERMISSION_DENIED);
         }
         // Regenerate salt
