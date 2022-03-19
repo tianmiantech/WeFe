3a95c4c520a52d061e5bfb54ce0cd100bf929b82
winter.zou <winter.zou@welab-inc.com>
2022-03-19 17:51:00 +0800

    update

---
+++ serving/serving-service/src/main/java/com/welab/wefe/serving/service/api/operation/LogQueryApi.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 Tianmian Tech. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.welab.wefe.serving.service.api.operation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.welab.wefe.common.exception.StatusCodeWithException;
+import com.welab.wefe.common.fieldvalidate.annotation.Check;
+import com.welab.wefe.common.web.api.base.AbstractApi;
+import com.welab.wefe.common.web.api.base.Api;
+import com.welab.wefe.common.web.dto.ApiResult;
+import com.welab.wefe.serving.service.dto.OperationLogOutputModel;
+import com.welab.wefe.serving.service.dto.PagingInput;
+import com.welab.wefe.serving.service.dto.PagingOutput;
+import com.welab.wefe.serving.service.service.OperationLogService;
+
+/**
+ * @author eval
+ **/
+@Api(path = "log/query", name = "query log")
+public class LogQueryApi extends AbstractApi<LogQueryApi.Input, PagingOutput<OperationLogOutputModel>> {
+
+    @Autowired
+    OperationLogService service;
+
+    @Override
+    protected ApiResult<PagingOutput<OperationLogOutputModel>> handle(Input input) throws StatusCodeWithException {
+        return success(service.query(input));
+    }
+
+    public static class Input extends PagingInput {
+        @Check(name = "请求接口")
+        public String logInterface;
+        @Check(name = "操作人员Id")
+        public String operatorId;
+        private Long startTime;
+        private Long endTime;
+
+        public Long getStartTime() {
+            return startTime;
+        }
+
+        public void setStartTime(Long startTime) {
+            this.startTime = startTime;
+        }
+
+        public Long getEndTime() {
+            return endTime;
+        }
+
+        public void setEndTime(Long endTime) {
+            this.endTime = endTime;
+        }
+    }
+}
