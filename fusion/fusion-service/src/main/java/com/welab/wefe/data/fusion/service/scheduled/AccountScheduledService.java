96f663186455a9bd2177f1858d341bb6407ca8fe
hunter.zhao <7GHMDBCENU5H>
2022-03-19 17:09:40 +0800

    wefe-fusion账号冻结定时器

--- fusion/fusion-service/src/main/java/com/welab/wefe/data/fusion/service/scheduled/AccountScheduledService.java
+++ fusion/fusion-service/src/main/java/com/welab/wefe/data/fusion/service/scheduled/AccountScheduledService.java
@@ -37,8 +37,8 @@ public class AccountScheduledService {
     @Autowired
     private AccountRepository accountRepository;
 
-//    @Scheduled(fixedDelay = 600_000, initialDelay = 10_000)
-    @Scheduled(fixedDelay = 5_000, initialDelay = 1_000)
+    @Scheduled(fixedDelay = 600_000, initialDelay = 10_000)
+//    @Scheduled(fixedDelay = 5_000, initialDelay = 1_000)
     public void run() {
 
         LOG.info("begin disableAccountWithoutAction90Days...");
