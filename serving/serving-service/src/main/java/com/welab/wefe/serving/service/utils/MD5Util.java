e5ff28482a5fe09bf5a69a648b26f063e5144916
hunter.zhao <7GHMDBCENU5H>
2022-04-08 14:55:52 +0800

    wefe-serving licenses修改

--- serving/serving-service/src/main/java/com/welab/wefe/serving/service/utils/MD5Util.java
+++ serving/serving-service/src/main/java/com/welab/wefe/serving/service/utils/MD5Util.java
@@ -1,12 +1,12 @@
-/**
+/*
  * Copyright 2021 Tianmian Tech. All Rights Reserved.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,19 +33,4 @@ public class MD5Util {
             return null;
         }
     }
-
-//    public static void main(String[] args) {
-//        PythonInterpreter interpreter = new PythonInterpreter();
-////		interpreter.exec("a=[5,2,3,9,4,0]");
-////		interpreter.exec("print(sorted(a));");
-////		interpreter.exec("print sorted(a);");
-//
-////        interpreter.execfile("/Users/hunter.zhao/Documents/temp/test.py");
-//        interpreter.execfile("/Users/hunter.zhao/Desktop/workspace/code/Serving-0.5.0/python/examples/yolov4/test_client.py");
-//
-//        PyFunction pyFunction = interpreter.get("predict", PyFunction.class);
-//        int a = 5, b = 6;
-//        PyObject pyobj = pyFunction.__call__(new PyInteger(a), new PyInteger(b));
-//        System.out.println(pyobj);
-//    }
 }
