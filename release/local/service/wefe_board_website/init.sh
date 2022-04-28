1bb8cceaef29b27b5aaaed1b1369f13ddc7f08d5
wingo.wen <wingo.wen@welab-inc.com>
2022-04-28 16:53:47 +0800

    init

---
+++ release/local/service/wefe_board_website/init.sh
@@ -0,0 +1,16 @@
+source ../deploy.cfg
+
+# 检查资源是否存在
+DIR_NAME=board-website
+
+if [ $1 == 'update' ];then
+  rm -rf $DIR_NAME $DIR_NAME.tar
+  wget $DOWNLOAD_BASE_PATH/PACKAGES/$DIR_NAME.tar
+fi
+
+tar -xvf $DIR_NAME.tar
+
+cd DIR_NAME/DIR_NAME/index.html
+
+
+
--- release/local/shell/pack.sh
+++ release/local/shell/pack.sh
@@ -3,23 +3,19 @@
 source ./env
 source ./func
 
-rebuild_cd_dir $WORK_DIR/package
-init_dir $INIT_DIR_STR
+rebuild_cd_dir $PACK_DIR
 
-cd $WORK_DIR
 pack_java $JAVA_PACK_STR
 
-cd $WORK_DIR
 JAR_PATH_STR=`get_jar_path $JAVA_PACK_STR`
 echo $JAR_PATH_STR
 
-cd $WORK_DIR
 copy_jar $JAR_PATH_STR
 
-cd $WORK_DIR
 source ~/.bash_profile
 nvm use 16.13.0
 pack_webs $WEB_PACK_STR
 
-cd $WORK_DIR
 pack_python
+
+upload_packages
