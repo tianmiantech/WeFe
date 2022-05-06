79b1166bf6ba91187dfd90b35502deaec3a56026
wingo.wen <wingo.wen@welab-inc.com>
2022-05-06 15:42:43 +0800

    init

--- release/local/service/wefe_fusion_website/init.sh
+++ release/local/service/wefe_fusion_website/init.sh
@@ -2,6 +2,10 @@ source ../deploy.cfg
 
 # 检查资源是否存在
 DIR_NAME=fusion-website
+CONTEXT=fusion-website
+BASE_RUL="\"$BOARD_SERVICE_URL\""
+PREFIX_PATH="\"/$CONTEXT/"\"
+echo $PREFIX_PATH
 
 if [ $1 == 'update' ];then
   rm -rf $DIR_NAME $DIR_NAME.tar
@@ -10,7 +14,8 @@ fi
 
 tar -xvf $DIR_NAME.tar
 
-cd DIR_NAME/DIR_NAME/index.html
+sed -i 's@</title>@&<script>window.clientApi = {env: "prod", baseUrl: "", prefixPath: ""};</script>@' $DIR_NAME/$CONTEXT/index.html
+sed -i "s@baseUrl.*,@baseUrl: $BASE_RUL,@" $DIR_NAME/$CONTEXT/index.html
+sed -i "s@prefixPath.*}@prefixPath: $PREFIX_PATH }@" $DIR_NAME/$CONTEXT/index.html
 
-
-
+echo "Success \nPlease config the nginx service."
