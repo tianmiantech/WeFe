b82f9d9e9d4005fa2c38b74fca9e8e905fce6724
ivenn.zheng <ivenn.zheng@welabb-inc.com>
2022-06-08 12:00:48 +0800

    feat: serving 初始化

--- board/board-website/src/views/system-config/system-config-view.vue
+++ board/board-website/src/views/system-config/system-config-view.vue
@@ -6,6 +6,8 @@
                 @submit.prevent
             >
                 <el-row :gutter="30">
+
+
                     <el-col :span="12">
                         <fieldset>
                             <legend>Board</legend>
@@ -53,28 +55,28 @@
                                     <template #content>
                                         <div class="rule-guide f12">
                                             <strong>tips：</strong>
-                                            <br />
+                                            <br/>
                                             所有需要访问 gateway
                                             的本地服务均需要将其 IP
-                                            地址添加到白名单，<br />
+                                            地址添加到白名单，<br/>
                                             目前有 board-service 和 flow
                                             两个服务需要访问
                                             gateway，白名单支持通配符和注释。
-                                            <br />
-                                            <br />
-                                            # 允许所有 IP 地址<br />
-                                            *<br />
-                                            <br />
-                                            # 允许指定网段<br />
-                                            10.90.*<br />
-                                            172.29.26.*<br />
-                                            <br />
-                                            # 精确指定<br />
+                                            <br/>
+                                            <br/>
+                                            # 允许所有 IP 地址<br/>
+                                            *<br/>
+                                            <br/>
+                                            # 允许指定网段<br/>
+                                            10.90.*<br/>
+                                            172.29.26.*<br/>
+                                            <br/>
+                                            # 精确指定<br/>
                                             183.3.218.18
                                         </div>
                                     </template>
                                     <el-icon class="el-icon-opportunity">
-                                        <elicon-opportunity />
+                                        <elicon-opportunity/>
                                     </el-icon>
                                 </el-tooltip>
                             </el-form-item>
@@ -96,7 +98,45 @@
                                     v-model="config.wefe_serving.intranet_base_uri"
                                 />
                             </el-form-item>
+                            <el-form-item>
+                                <el-button type="success"
+                                           @click="showDialog"
+                                >
+                                    初始化
+                                </el-button>
+                            </el-form-item>
+
+
                         </fieldset>
+                        <el-dialog title="Serving系统初始化" v-model="dialogVisibleInfo" width="40%">
+                            <el-form :model="form">
+                                <el-form-item label="账号" :label-width="formLabelWidth">
+                                    <el-input v-model="form.phone_number" type="text" clearable
+                                              placeholder="请输入手机号"></el-input>
+                                </el-form-item>
+                                <el-form-item label="密码" :label-width="formLabelWidth">
+                                    <el-input v-model="form.password" type="password" clearable
+                                              placeholder="请输入密码"></el-input>
+                                </el-form-item>
+                            </el-form>
+                            <div class="dialog-footer">
+                                <el-button @click="dialogVisibleInfo = false">取 消</el-button>
+                                <el-button type="primary" @click="init">确 定</el-button>
+                            </div>
+                        </el-dialog>
+
+                        <el-dialog
+                            title="成功"
+                            v-model="initDialogVisible"
+                            width="30%"
+                            center
+                            custom-class="init-success"
+                        >
+                            <div class="serving-init-div">Serving 系统已初始化成功！</div>
+                            <div style="text-align: right">
+                                <el-button type="primary" @click="initDialogVisible = false">确 定</el-button>
+                            </div>
+                        </el-dialog>
                     </el-col>
                     <el-col :span="12">
                         <fieldset>
@@ -113,13 +153,13 @@
                         <fieldset>
                             <legend>邮件服务器</legend>
                             <el-form-item label="邮件服务器地址：">
-                                <el-input v-model="config.mail_server.mail_host" />
+                                <el-input v-model="config.mail_server.mail_host"/>
                             </el-form-item>
                             <el-form-item label="邮件服务器端口：">
-                                <el-input v-model="config.mail_server.mail_port" />
+                                <el-input v-model="config.mail_server.mail_port"/>
                             </el-form-item>
                             <el-form-item label="邮件用户名：">
-                                <el-input v-model="config.mail_server.mail_username" />
+                                <el-input v-model="config.mail_server.mail_username"/>
                             </el-form-item>
                             <el-form-item label="邮件密码：">
                                 <el-input
@@ -135,15 +175,10 @@
                         </fieldset>
                     </el-col>
                 </el-row>
+
+
                 <el-divider/>
-                <el-row :gutter="30">
-                    <el-col :span="12">
 
-                    </el-col>
-                    <el-col :span="12">
-
-                    </el-col>
-                </el-row>
                 <el-button
                     v-loading="loading"
                     class="save-btn mt10"
@@ -160,6 +195,7 @@
 
 <script>
     import { mapGetters } from 'vuex';
+    import md5 from 'js-md5';
 
     export default {
         data() {
@@ -174,7 +210,14 @@
                     alert_config: {},
                     mail_server:  {},
                 },
-                visible: true,
+                form: {
+                    phone_number: '',
+                    password:     '',
+                },
+                visible:           true,
+                dialogVisibleInfo: false,
+                formLabelWidth:    '20px',
+                initDialogVisible: false,
             };
         },
         computed: {
@@ -184,11 +227,45 @@
             this.getData();
         },
         methods: {
+            showDialog() {
+                this.dialogVisibleInfo = true;
+            },
+
+            async init() {
+
+                const password = [
+                    this.form.phone_number,
+                    this.form.password,
+                    this.form.phone_number,
+                    this.form.phone_number.substr(0, 3),
+                    this.form.password.substr(this.form.password.length - 3),
+                ].join('');
+
+                const { code, data, message } = await this.$http.post({
+                    url:  '/member/sync_to_serving',
+                    data: {
+                        phone_number: this.form.phone_number,
+                        password:     md5(password),
+                    },
+                });
+
+                if (code === 0) {
+                    console.log(data);
+                    console.log(message);
+                    this.initDialogVisible = true;
+                } else {
+                    console.log(data);
+                    console.log(message);
+                }
+
+            },
+
+
             async getData() {
                 this.loading = true;
                 const { code, data } = await this.$http.post({
                     url:  '/global_config/get',
-                    data: { groups: ['wefe_board','wefe_gateway','alert_config','mail_server', 'wefe_flow', 'wefe_serving'] },
+                    data: { groups: ['wefe_board', 'wefe_gateway', 'alert_config', 'mail_server', 'wefe_flow', 'wefe_serving'] },
                 });
 
                 if (code === 0) {
@@ -215,20 +292,36 @@
 </script>
 
 <style lang="scss" scoped>
-    .el-icon-opportunity {
-        font-size: 16px;
-        color: $--color-warning;
-        position: absolute;
-        right: 10px;
-        top: 10px;
+.el-icon-opportunity {
+    font-size: 16px;
+    color: $--color-warning;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+}
+
+.rule-guide {
+    color: #6c757d;
+    line-height: 20px;
+    border-radius: 4px;
+    padding: 5px 10px;
+}
+
+.save-btn {
+    width: 100px;
+}
+
+.serving-init-div {
+    padding: 5px;
+    margin-bottom: 10px;
+}
+
+.init-success {
+    .el-dialog__body {
+        padding: 20px;
     }
-    .rule-guide {
-        color: #6c757d;
-        line-height: 20px;
-        border-radius: 4px;
-        padding: 5px 10px;
-    }
-    .save-btn {
-        width: 100px;
-    }
+}
 </style>
+<!--<style lang="scss">
+
+</style>-->
