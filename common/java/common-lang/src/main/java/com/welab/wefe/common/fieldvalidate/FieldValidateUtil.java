76d250683bf8ce778da458cdc507ea401cc963b3
yuxin.zhang <yuxin.zhang@welab-inc.com>
2022-06-14 11:30:05 +0800

    日志输出,添加value过滤器,防止日志内容太大

--- common/java/common-lang/src/main/java/com/welab/wefe/common/fieldvalidate/FieldValidateUtil.java
+++ common/java/common-lang/src/main/java/com/welab/wefe/common/fieldvalidate/FieldValidateUtil.java
@@ -19,7 +19,7 @@ package com.welab.wefe.common.fieldvalidate;
 import com.alibaba.fastjson.JSON;
 import com.welab.wefe.common.StatusCode;
 import com.welab.wefe.common.exception.StatusCodeWithException;
-import com.welab.wefe.common.fastjson.LoggerSerializeConfig;
+import com.welab.wefe.common.fastjson.LoggerValueFilter;
 import com.welab.wefe.common.fieldvalidate.annotation.Check;
 import com.welab.wefe.common.util.ClassUtils;
 import com.welab.wefe.common.util.StringUtil;
@@ -156,7 +156,7 @@ public class FieldValidateUtil {
         }
 
         String valueStr = value instanceof Map
-                ? JSON.toJSONString(value, LoggerSerializeConfig.instance())
+                ? JSON.toJSONString(value, LoggerValueFilter.instance)
                 : value.toString();
 
         String keyword = ReactionaryKeywords.match(valueStr);
@@ -176,7 +176,7 @@ public class FieldValidateUtil {
         }
 
         String valueStr = value instanceof Map
-                ? JSON.toJSONString(value, LoggerSerializeConfig.instance())
+                ? JSON.toJSONString(value, LoggerValueFilter.instance)
                 : value.toString();
 
         for (String keyword : XSS_KEYWORDS) {
