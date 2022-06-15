2e51a3037a0d2716d78811e83b90d19a77b083da
jensen.liang <jensen.liang@welab-inc.com>
2022-06-15 14:19:57 +0800

    修复衍生数据集未展示正负样本比例的问题

--- kernel/tracker/tracking.py
+++ kernel/tracker/tracking.py
@@ -660,11 +660,11 @@ class Tracking(object):
         else:
             table_data_set.column_name_list = f"{table_data_set.primary_key_column},{table_data_set.y_name_list},{','.join(header_list)}"
 
-            # y positive count
-            y_positive_count = data_table.filter(lambda k, v: int(v.label) > 0).count()
-            y_positive_ratio = round(y_positive_count / data_input['table_create_count'], 4)
-            table_data_set.y_positive_example_count = y_positive_count
-            table_data_set.y_positive_example_ratio = y_positive_ratio
+        # y positive count
+        y_positive_count = data_table.filter(lambda k, v: int(v.label) > 0).count()
+        y_positive_ratio = round(y_positive_count / data_input['table_create_count'], 4)
+        table_data_set.y_positive_example_count = y_positive_count
+        table_data_set.y_positive_example_ratio = y_positive_ratio
 
         if len(header_list) == 0:
             table_data_set.column_name_list = table_data_set.column_name_list[1:]
