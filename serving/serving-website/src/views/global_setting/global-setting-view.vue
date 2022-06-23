6949d07db48583c13594df3f28ffd2e6413a4b27
ivenn.zheng <ivenn.zheng@welabb-inc.com>
2022-06-23 16:56:51 +0800

    feat: 修改全局配置样式，添加是否加入联邦提示

--- serving/serving-website/src/views/global_setting/global-setting-view.vue
+++ serving/serving-website/src/views/global_setting/global-setting-view.vue
@@ -1,121 +1,156 @@
 <template>
-    <el-card
-        class="page"
-    >
-        <el-form :model="form">
-            <el-row :gutter="24">
-                <el-col :span="10">
+    <div class="page">
+        <el-card>
+            <el-form :model="form" @submit.prevent>
+                <el-row :gutter="24">
+                    <el-col :span="10">
 
-                    <fieldset>
-                        <legend>基本配置</legend>
+                        <fieldset>
+                            <legend>基本配置</legend>
 
-                        <el-form-item label="系统ID：">
-                            {{ form.identity_info.member_id }}
-                        </el-form-item>
-                        <el-form-item
-                            :rules="[{required: true, message: '名称必填!'}]"
-                            label="成员名称："
-                        >
-                            <el-input
-                                v-model="form.identity_info.member_name"
-                                placeholder="仅支持中文名称"
-                                :disabled="is_update"
-                            />
-                        </el-form-item>
-                        <el-form-item
-                            label="Serving服务地址："
-                        >
-                            <el-input
-                                v-model="form.identity_info.serving_base_url"
-                                placeholder=""
-                                :disabled="is_update"
-                            />
-                        </el-form-item>
+                            <el-form-item label="系统ID：">
+                                {{ form.identity_info.member_id }}
 
-                        <el-form-item
-                            label="Union服务地址："
-                        >
-                            <el-input
-                                v-model="form.wefe_union.intranet_base_uri"
-                                placeholder=""
-                                :disabled="is_update"
-                            />
-                        </el-form-item>
+                                <el-popover v-if="mode === 1"
+                                    placement="top-start"
+                                    width="100"
+                                    trigger="hover"
+                                    content="已加入联邦"
+                                >
+                                    <el-button
+                                        slot="reference"
+                                        size="medium"
+                                        icon="el-icon-question"
+                                        type="text"
+                                    ></el-button>
+                                </el-popover>
 
-                        <el-form-item label="运行模式：">
-                            <el-radio v-model="mode" :label="0" @change="modeChange">独立模式</el-radio>
-                            <el-radio v-model="mode" :label="1" @change="modeChange">联邦模式</el-radio>
-                        </el-form-item>
-                    </fieldset>
-                    <fieldset>
-                        <legend>邮箱配置</legend>
+                                <el-popover v-if="mode === 0"
+                                            placement="top-start"
+                                            width="100"
+                                            trigger="hover"
+                                            content="未加入联邦！如需加入联邦，请部署wefe-board系统，并将同步密钥到serving（全局设置->系统设置->同步）">
+                                    <el-button
+                                        slot="reference"
+                                        size="medium"
+                                        icon="el-icon-question"
+                                        type="text"
+                                    ></el-button>
+                                </el-popover>
 
-                        <el-form-item
-                            label="邮件服务器地址："
-                        >
-                            <el-input
-                                v-model="form.mail_server.host"
-                                placeholder=""
-                                :disabled="is_update"
-                            />
-                        </el-form-item>
-                        <el-form-item
-                            label="邮件服务器端口："
-                        >
-                            <el-input
-                                v-model="form.mail_server.port"
-                                placeholder=""
-                                :disabled="is_update"
-                            />
-                        </el-form-item>
-                        <el-form-item
-                            label="邮件用户名："
-                        >
-                            <el-input
-                                v-model="form.mail_server.username"
-                                placeholder=""
-                                :disabled="is_update"
-                            />
-                        </el-form-item>
-                        <el-form-item
-                            label="邮件密码："
-                        >
-                            <el-input
-                                type="password"
-                                v-model="form.mail_server.password"
-                                placeholder=""
-                                :disabled="is_update"
-                            />
-                        </el-form-item>
+                            </el-form-item>
+                            <el-form-item
+                                :rules="[{required: true, message: '名称必填!'}]"
+                                label="成员名称："
+                            >
+                                <el-input
+                                    v-model="form.identity_info.member_name"
+                                    placeholder="仅支持中文名称"
+                                    :disabled="is_update"
+                                />
+                            </el-form-item>
+                            <el-form-item
+                                label="Serving服务地址："
+                            >
+                                <el-input
+                                    v-model="form.identity_info.serving_base_url"
+                                    placeholder=""
+                                    :disabled="is_update"
+                                />
+                            </el-form-item>
+
+                            <el-form-item
+                                label="Union服务地址："
+                            >
+                                <el-input
+                                    v-model="form.wefe_union.intranet_base_uri"
+                                    placeholder=""
+                                    :disabled="is_update"
+                                />
+                            </el-form-item>
+
+<!--                            <el-form-item label="运行模式：">-->
+<!--                                <el-radio v-model="mode" :label="0" @change="modeChange">独立模式</el-radio>-->
+<!--                                <el-radio v-model="mode" :label="1" @change="modeChange">联邦模式</el-radio>-->
+<!--                            </el-form-item>-->
+                        </fieldset>
+                        <fieldset>
+                            <legend>邮箱配置</legend>
+
+                            <el-form-item
+                                label="邮件服务器地址："
+                            >
+                                <el-input
+                                    v-model="form.mail_server.host"
+                                    placeholder=""
+                                    :disabled="is_update"
+                                />
+                            </el-form-item>
+                            <el-form-item
+                                label="邮件服务器端口："
+                            >
+                                <el-input
+                                    v-model="form.mail_server.port"
+                                    placeholder=""
+                                    :disabled="is_update"
+                                />
+                            </el-form-item>
+                            <el-form-item
+                                label="邮件用户名："
+                            >
+                                <el-input
+                                    v-model="form.mail_server.username"
+                                    placeholder=""
+                                    :disabled="is_update"
+                                />
+                            </el-form-item>
+                            <el-form-item
+                                label="邮件密码："
+                            >
+                                <el-input
+                                    type="password"
+                                    v-model="form.mail_server.password"
+                                    placeholder=""
+                                    :disabled="is_update"
+                                />
+                            </el-form-item>
 
 
-                    </fieldset>
-                </el-col>
-            </el-row>
+                        </fieldset>
+                    </el-col>
+                </el-row>
 
-            <el-divider/>
-            <el-button
-                v-loading="loading"
-                class="save-btn"
-                type="primary"
-                size="medium"
-                :disabled="is_update"
-                @click="update"
-            >
-                更新
+                <el-divider/>
+                <el-button
+                    v-loading="loading"
+                    class="save-btn"
+                    type="primary"
+                    size="medium"
+                    :disabled="is_update"
+                    @click="update"
+                >
+                    更新
+                </el-button>
+
+            </el-form>
+        </el-card>
+        <el-card v-if="userInfo.super_admin_role" class="mt20">
+            <el-alert
+                title="重置密钥："
+                description="Serving 系统的秘钥对，用于系统对外的加签验证"
+                style="max-width:600px;"
+                type="info"
+                close-text=" "
+                show-icon
+            />
+            <br />
+            <el-button type="danger" v-loading="is_reset" @click="resetKey">
+                重置密钥
             </el-button>
+        </el-card>
 
-            <el-button
-                v-loading="is_reset"
-                class="save-btn"
-                type="primary"
-                size="medium"
-                @click="resetKey"
-            >重置公私钥
-            </el-button>
+    </div>
 
-        </el-form>
-    </el-card>
 </template>
 
 <script>
@@ -124,6 +159,7 @@ import {mapGetters} from 'vuex';
 export default {
     data() {
         return {
+
             loading: false,
             is_update: false,
             is_display: false,
