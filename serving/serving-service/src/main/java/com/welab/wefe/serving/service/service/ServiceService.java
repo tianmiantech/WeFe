25d05ca2d0ac55b13924fb143ca1f130deab54e4
winter.zou <winter.zou@welab-inc.com>
2022-08-02 14:09:00 +0800

    update

--- serving/serving-service/src/main/java/com/welab/wefe/serving/service/service/ServiceService.java
+++ serving/serving-service/src/main/java/com/welab/wefe/serving/service/service/ServiceService.java
@@ -713,8 +713,11 @@ public class ServiceService {
         PartnerMysqlModel partner = partnerService.queryByCode(input.getPartnerCode());
 
         ServiceOrderMysqlModel serviceOrderModel = serviceOrderService.add(service.getId(), service.getName(),
-                service.getServiceType(), CallByMeEnum.NO.getValue(), status.getValue(), partner.getId(),
-                partner.getName(), CacheObjects.getMemberId(), CacheObjects.getMemberName());
+                service.getServiceType(),
+                input.getPartnerCode().equalsIgnoreCase(CacheObjects.getMemberId()) ? CallByMeEnum.YES.getValue()
+                        : CallByMeEnum.NO.getValue(),
+                status.getValue(), partner.getId(), partner.getName(), CacheObjects.getMemberId(),
+                CacheObjects.getMemberName());
         return serviceOrderModel.getId();
     }
 
@@ -735,7 +738,9 @@ public class ServiceService {
         callLog.setResponsePartnerId(CacheObjects.getMemberId());
         callLog.setResponsePartnerName(CacheObjects.getMemberName());
         callLog.setResponseData(JSON.toJSONString(result));
-        callLog.setCallByMe(CallByMeEnum.NO.getValue());
+        callLog.setCallByMe(
+                input.getPartnerCode().equalsIgnoreCase(CacheObjects.getMemberId()) ? CallByMeEnum.YES.getValue()
+                        : CallByMeEnum.NO.getValue());
         callLog.setResponseStatus(responseStatus);
         callLog.setSpendTime(System.currentTimeMillis() - beginTime);
         serviceCallLogService.save(callLog);
