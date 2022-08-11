32bd1ecd00e58f4c5fac2b11a561de7361f888f0
Nova.li <Nova.Li@welab-inc.com>
2022-08-11 15:35:46 +0800

    test.

--- manager/manager-website/src/router/routes.js
+++ manager/manager-website/src/router/routes.js
@@ -110,34 +110,34 @@ const baseRoutes = [
                 },
                 component: () => import('../views/cert/cert-list'),
             },
-            {
-                path: `${prefixPath}cert-view`,
-                name: 'cert-view',
-                meta: {
-                    loginAndRefresh: true,
-                    hidden:          true,
-                    title:           '证书详情',
-                },
-                component: () => import('../views/cert/cert-view'),
-            },
-            {
-                path: `${prefixPath}csr-list`,
-                name: 'csr-list',
-                meta: {
-                    loginAndRefresh: true,
-                    title:           '证书请求列表',
-                },
-                component: () => import('../views/cert/csr-list'),
-            },
-            {
-                path: `${prefixPath}cert-key-list`,
-                name: 'cert-key-list',
-                meta: {
-                    loginAndRefresh: true,
-                    title:           '私钥列表',
-                },
-                component: () => import('../views/cert/cert-key-list'),
-            },
+            // {
+            //     path: `${prefixPath}cert-view`,
+            //     name: 'cert-view',
+            //     meta: {
+            //         loginAndRefresh: true,
+            //         hidden:          true,
+            //         title:           '证书详情',
+            //     },
+            //     component: () => import('../views/cert/cert-view'),
+            // },
+            // {
+            //     path: `${prefixPath}csr-list`,
+            //     name: 'csr-list',
+            //     meta: {
+            //         loginAndRefresh: true,
+            //         title:           '证书请求列表',
+            //     },
+            //     component: () => import('../views/cert/csr-list'),
+            // },
+            // {
+            //     path: `${prefixPath}cert-key-list`,
+            //     name: 'cert-key-list',
+            //     meta: {
+            //         loginAndRefresh: true,
+            //         title:           '私钥列表',
+            //     },
+            //     component: () => import('../views/cert/cert-key-list'),
+            // },
         ],
     },
     {
