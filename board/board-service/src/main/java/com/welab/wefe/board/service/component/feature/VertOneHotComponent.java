304f456d75b8e1b257de9f4d9ea3a69789bd020b
winter.zou <winter.zou@welab-inc.com>
2022-09-30 09:30:34 +0800

    update

--- board/board-service/src/main/java/com/welab/wefe/board/service/component/feature/VertOneHotComponent.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/component/feature/VertOneHotComponent.java
@@ -29,34 +29,32 @@ import com.welab.wefe.board.service.component.base.io.IODataType;
 import com.welab.wefe.board.service.component.base.io.InputMatcher;
 import com.welab.wefe.board.service.component.base.io.Names;
 import com.welab.wefe.board.service.component.base.io.OutputItem;
-import com.welab.wefe.board.service.component.feature.VertOneHotComponent.Params.MemberInfoModel;
+import com.welab.wefe.board.service.component.feature.HorzOneHotComponent.Params;
+import com.welab.wefe.board.service.component.feature.HorzOneHotComponent.Params.MemberInfoModel;
 import com.welab.wefe.board.service.database.entity.job.TaskMySqlModel;
 import com.welab.wefe.board.service.database.entity.job.TaskResultMySqlModel;
-import com.welab.wefe.board.service.dto.entity.MemberModel;
 import com.welab.wefe.board.service.exception.FlowNodeException;
 import com.welab.wefe.board.service.model.FlowGraph;
 import com.welab.wefe.board.service.model.FlowGraphNode;
 import com.welab.wefe.board.service.model.JobBuilder;
 import com.welab.wefe.board.service.service.CacheObjects;
 import com.welab.wefe.common.exception.StatusCodeWithException;
-import com.welab.wefe.common.fieldvalidate.annotation.Check;
 import com.welab.wefe.common.util.JObject;
-import com.welab.wefe.common.web.dto.AbstractApiInput;
 import com.welab.wefe.common.wefe.enums.ComponentType;
 
 @Service
-public class VertOneHotComponent extends AbstractComponent<VertOneHotComponent.Params> {
+public class VertOneHotComponent extends AbstractComponent<HorzOneHotComponent.Params> {
 	
 	@Override
 	protected void checkBeforeBuildTask(FlowGraph graph, List<TaskMySqlModel> preTasks, FlowGraphNode node,
-			VertOneHotComponent.Params params) throws FlowNodeException {
+	        HorzOneHotComponent.Params params) throws FlowNodeException {
 	}
 
 	@Override
 	protected JSONObject createTaskParams(JobBuilder jobBuilder, FlowGraph graph, List<TaskMySqlModel> preTasks, FlowGraphNode node,
-										  VertOneHotComponent.Params params) throws FlowNodeException {
+	        HorzOneHotComponent.Params params) throws FlowNodeException {
 		JSONObject taskParam = new JSONObject();
-		List<VertOneHotComponent.Params.MemberInfoModel> members = params.getMembers();
+		List<HorzOneHotComponent.Params.MemberInfoModel> members = params.getMembers();
 
 		List<String> transformColNames = new ArrayList<>();
 		members.forEach(member -> {
@@ -102,9 +100,9 @@ public class VertOneHotComponent extends AbstractComponent<VertOneHotComponent.P
         Params params = (Params) node.getParamsModel();
 
         // When feature is selected,stop creating after task.
-        if (CollectionUtils.isNotEmpty(params.members) && params.members.size() > 0) {
+        if (CollectionUtils.isNotEmpty(params.getMembers()) && params.getMembers().size() > 0) {
             boolean selectFeature = false;
-            for (MemberInfoModel member : params.members) {
+            for (MemberInfoModel member : params.getMembers()) {
                 if (CollectionUtils.isNotEmpty(member.getFeatures()) && member.getFeatures().size() > 0) {
                     selectFeature = true;
                 }
@@ -125,33 +123,5 @@ public class VertOneHotComponent extends AbstractComponent<VertOneHotComponent.P
 	@Override
 	public List<OutputItem> outputs(FlowGraph graph, FlowGraphNode node) throws FlowNodeException {
 		return Arrays.asList(OutputItem.of(Names.Data.NORMAL_DATA_SET, IODataType.DataSetInstance));
-	}
-	
-
-	public static class Params extends AbstractApiInput {
-
-		@Check(name = "成员信息", require = true)
-		private List<MemberInfoModel> members;
-
-		public List<MemberInfoModel> getMembers() {
-			return members;
-		}
-
-		public void setMembers(List<MemberInfoModel> members) {
-			this.members = members;
-		}
-
-		public static class MemberInfoModel extends MemberModel {
-			@Check(name = "特征列", require = true)
-			private List<String> features = new ArrayList<>();
-
-			public List<String> getFeatures() {
-				return features;
-			}
-
-			public void setFeatures(List<String> features) {
-				this.features = features;
-			}
-		}
 	}
 }
