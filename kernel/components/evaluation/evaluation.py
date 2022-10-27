f3d2c2471989393d68967aae2f256bb06f02f741
aisen <1431371354@qq.com>
2022-10-27 12:43:11 +0800

    eval_bin_psi_code_base_fat3.1

--- kernel/components/evaluation/evaluation.py
+++ kernel/components/evaluation/evaluation.py
@@ -952,8 +952,9 @@ class Evaluation(ModelBase):
             pred_result[mode] = pred_scores
         train_pred_score = pred_result.get('train')
         eval_pred_score = pred_result.get('validate')
-        if eval_pred_score and train_pred_score:
+        if eval_pred_score and train_pred_score and self.component_parameters['module']=='Evaluation':
             train_bin_values, train_split_point = self.get_bin_result(train_pred_score)
+            LOGGER.debug('train_bin_values and train_split_point'.format(train_bin_values, train_split_point))
             train_bin_results = self.cal_bin_rate(train_bin_values)
             eval_bin_values = pd.cut(eval_pred_score, train_split_point)
             eval_bin_results = self.cal_bin_rate(eval_bin_values)
