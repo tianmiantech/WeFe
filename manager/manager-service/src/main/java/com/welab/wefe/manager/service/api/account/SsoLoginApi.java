773b5be21a5a6e774ba4694a0e111cef7504bb65
aaron.li <aaron.li@welab-inc.com>
2022-10-28 17:57:26 +0800

    Manager：新增获取当前登录用户SsoLoginApi接口

---
+++ manager/manager-service/src/main/java/com/welab/wefe/manager/service/api/account/SsoLoginApi.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 Tianmian Tech. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.welab.wefe.manager.service.api.account;
+
+import com.welab.wefe.common.exception.StatusCodeWithException;
+import com.welab.wefe.common.web.api.base.AbstractNoneInputApi;
+import com.welab.wefe.common.web.api.base.Api;
+import com.welab.wefe.common.web.dto.AbstractApiOutput;
+import com.welab.wefe.common.web.dto.ApiResult;
+import com.welab.wefe.common.web.service.account.AccountInfo2;
+import com.welab.wefe.common.web.util.CurrentAccountUtil;
+
+
+@Api(path = "account/sso_login", name = "sso_login", login = false)
+public class SsoLoginApi extends AbstractNoneInputApi<SsoLoginApi.Output> {
+
+    @Override
+    protected ApiResult<Output> handle() throws StatusCodeWithException {
+        AccountInfo2 accountInfo = CurrentAccountUtil.get();
+        Output output = new Output();
+        output.setId(accountInfo.getId());
+        output.setPhoneNumber(accountInfo.getPhoneNumber());
+        output.setNickname(accountInfo.getName());
+
+        return success(output);
+    }
+
+    public static class Output extends AbstractApiOutput {
+        private String id;
+        private String phoneNumber;
+        private String nickname;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
+    }
+}
