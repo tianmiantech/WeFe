4864c7a233d5db076a072e6ee60f87843d87904f
winter.zou <winter.zou@welab-inc.com>
2022-11-09 16:07:28 +0800

    update

--- serving/serving-service/src/main/java/com/welab/wefe/serving/service/service_processor/MultiPirServiceProcessor.java
+++ serving/serving-service/src/main/java/com/welab/wefe/serving/service/service_processor/MultiPirServiceProcessor.java
@@ -93,7 +93,6 @@ public class MultiPirServiceProcessor extends AbstractServiceProcessor<TableServ
                 throw e;
             }
         }
-
         return JObject.create("result", results);
     }
 }
