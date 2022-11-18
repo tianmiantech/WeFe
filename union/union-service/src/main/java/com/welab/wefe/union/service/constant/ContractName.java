a0317bf7af6d36ca1bda39a5ac9da89f550a7b3a
aaron.li <aaron.li@welab-inc.com>
2022-11-18 17:02:40 +0800

    合约名称由配置文件迁移到代码上

---
+++ union/union-service/src/main/java/com/welab/wefe/union/service/constant/ContractName.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 Tianmian Tech. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.welab.wefe.union.service.constant;
+
+/**
+ * 合约名
+ */
+public class ContractName {
+    /**
+     * 成员
+     */
+    public final static String MEMBER_CONTRACT = "MemberContract";
+    /**
+     * 数据集
+     */
+    public final static String DATA_SET_CONTRACT = "DataSetContract";
+    /**
+     * 成员数据集权限
+     */
+    public final static String DATA_SET_MEMBER_PERMISSION_CONTRACT = "DataSetMemberPermissionContract";
+    /**
+     * 成员实名类型
+     */
+    public final static String MEMBER_AUTH_TYPE_CONTRACT = "MemberAuthTypeContract";
+    /**
+     * Union节点
+     */
+    public final static String UNION_NODE_CONTRACT = "UnionNodeContract";
+    /**
+     * 成员文件
+     */
+    public final static String MEMBER_FILE_INFO_CONTRACT = "MemberFileInfoContract";
+    /**
+     * 成员服务
+     */
+    public final static String MEMBER_SERVICE_CONTRACT = "MemberServiceContract";
+    /**
+     * 布隆过滤器
+     */
+    public final static String BLOOM_FILTER_CONTRACT = "BloomFilterContract";
+    /**
+     * 数据源
+     */
+    public final static String DATA_RESOURCE_CONTRACT = "DataResourceContract";
+    /**
+     * 图片资源
+     */
+    public final static String IMAGE_DATA_SET_CONTRACT = "ImageDataSetContract";
+    /**
+     * 普通二维表资源
+     */
+    public final static String TABLE_DATA_SET_CONTRACT = "TableDataSetContract";
+}
