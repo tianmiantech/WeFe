d567fee3148702597680e1b5f8c5d9da43ae1d72
winter.zou <winter.zou@welab-inc.com>
2022-11-18 17:55:50 +0800

    update

--- fusion/fusion-service/src/main/java/com/welab/wefe/data/fusion/service/service/bloomfilter/BloomFilterAddServiceDataRowConsumer.java
+++ fusion/fusion-service/src/main/java/com/welab/wefe/data/fusion/service/service/bloomfilter/BloomFilterAddServiceDataRowConsumer.java
@@ -217,7 +217,7 @@ public class BloomFilterAddServiceDataRowConsumer implements Consumer<Map<String
         long start = System.currentTimeMillis();
         LOG.info("generateFilter begin , size = " + rows.size());
         // 创建定长线程池
-        int poolSize = 20;
+        int poolSize = 50;
         ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(poolSize);
         final BloomFilterAddServiceDataRowConsumer consumer = this;
         for (Map<String, Object> data : rows) {
