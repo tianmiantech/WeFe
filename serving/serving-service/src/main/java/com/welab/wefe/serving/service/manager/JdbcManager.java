fd1fd23783b60d7464700fbb5b94a63f86899758
winter.zou <winter.zou@welab-inc.com>
2022-11-21 09:40:34 +0800

    update

--- serving/serving-service/src/main/java/com/welab/wefe/serving/service/manager/JdbcManager.java
+++ serving/serving-service/src/main/java/com/welab/wefe/serving/service/manager/JdbcManager.java
@@ -181,11 +181,11 @@ public class JdbcManager {
 		ResultSet rs = null;
 		List<Map<String, String>> result = new ArrayList<>();
 		try {
-//			ps = conn.prepareStatement(sql);
+			ps = conn.prepareStatement(sql);
 		    // 使用流式获取数据
-            ps = conn.prepareStatement(sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
-            ps.setFetchSize(Integer.MIN_VALUE);
-            ps.setLargeMaxRows(50000000);
+//            ps = conn.prepareStatement(sql, ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+//            ps.setFetchSize(Integer.MIN_VALUE);
+//            ps.setLargeMaxRows(50000000);
 			rs = ps.executeQuery();
 			while (rs.next()) {
 				Map<String, String> fieldMap = new LinkedHashMap<>();
