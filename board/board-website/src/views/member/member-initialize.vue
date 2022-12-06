c986add184962010c62669321605b9bfcc0c0c3c
mirone.wang <mirone.wang@sz.welab.com>
2022-12-06 14:10:50 +0800

    修改默认加密方式为sm2

--- board/board-website/src/views/member/member-initialize.vue
+++ board/board-website/src/views/member/member-initialize.vue
@@ -157,7 +157,7 @@
                     member_mobile:                '',
                     member_allow_public_data_set: true,
                     // gateway_uri:                  '',
-                    secret_key_type:              'rsa',
+                    secret_key_type:              'sm2',
                 },
                 memberCard: {
                     visible:    false,
