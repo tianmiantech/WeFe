55b51ff2cc3869cb865f61af1502a74fe1f94225
winter.zou <winter.zou@welab-inc.com>
2022-12-13 14:16:48 +0800

    update sdk

--- serving/serving-service/sdk_dir/PsiClient.java
+++ serving/serving-service/sdk_dir/PsiClient.java
@@ -14,37 +14,48 @@
  * limitations under the License.
  */
 
-import com.alibaba.fastjson.JSONObject;
-import com.welab.wefe.mpc.config.CommunicationConfig;
-import com.welab.wefe.mpc.psi.sdk.PrivateSetIntersection;
+package com.welab.wefe.mpc.psi.sdk;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
-// 两方交集查询 psi 
-// 配合 mpc-psi-sdk-1.0.0.jar使用
+import com.welab.wefe.mpc.config.CommunicationConfig;
+
+//两方交集查询 psi 
+//配合 mpc-psi-sdk-1.0.0.jar使用
 public class PsiClient {
     // 私钥
-    private static final String 测试客户1_privateKey = "***"; // TODO
+    private static final String 测试客户1_privateKey = "***";
     // 公钥
-    private static final String 测试客户1_publicKey = "***"; // TODO
+    private static final String 测试客户1_publicKey = "***";
     // 客户code
-    private static final String 测试客户1_code = "TEST***25"; // TODO
+    private static final String 测试客户1_code = "***";
     // Serving服务地址
-    private static final String serverUrl = "https://****/serving-service-01/"; // TODO
+    private static final String serverUrl = "https://xxx.xxx.com/xxx/"; // TODO
     // Service Api name
-    private static final String apiName = "api/*****"; // TODO
+    private static final String apiName = "api/xxx/xxx"; // TODO
+
+    private static List<String> clientDataset;
+
+    /**
+     * 初始化客户端数据
+     */
+    private static void initClientDataset() {
+        clientDataset = new ArrayList<>();
+        int total = 10000;
+        while ((total--) > 0) {
+            clientDataset.add(getMD5String("" + total * 10));
+        }
+    }
 
     public static void main(String[] args) throws Exception {
-        // params
-        JSONObject data = new JSONObject();
-        data.put("phone_number", "******");
-        String clientId = getMD5String(data.getString("phone_number"));
-        List<String> clientIds = Arrays.asList(new String[] { clientId });
+        initClientDataset();
 
-        PrivateSetIntersection privateSetIntersection = new PrivateSetIntersection();
+        long start = System.currentTimeMillis();
+        // params
+        Psi psi = PsiFactory.generatePsi("dh"); // dh or ecdh
         CommunicationConfig config = new CommunicationConfig();
         config.setNeedSign(true); // 是否需要签名
         config.setSignPrivateKey(测试客户1_privateKey);// 私钥
@@ -52,17 +63,18 @@ public class PsiClient {
         // 服务地址
         config.setServerUrl(serverUrl);
         config.setApiName(apiName);
-
-        List<String> result = privateSetIntersection.query(config, clientIds, 1024);
-
+        List<String> result = psi.query(config, clientDataset);
         // 如果 clientId = result 说明有结果
-        System.out.println(clientId);
-        System.out.println(result);
+        System.out.println(clientDataset.size());
+        System.out.println(result.size());
+        System.out.println("duration = " + (System.currentTimeMillis() - start));
     }
 
     public static String getMD5String(String str) {
         try {
+            // 生成一个MD5加密计算摘要
             MessageDigest md = MessageDigest.getInstance("MD5");
+            // 计算md5函数
             md.update(str.getBytes());
             return new BigInteger(1, md.digest()).toString(16);
         } catch (Exception e) {
@@ -73,7 +85,9 @@ public class PsiClient {
 
     public static String getSHA256String(String str) {
         try {
+            // 生成一个MD5加密计算摘要
             MessageDigest md = MessageDigest.getInstance("SHA-256");
+            // 计算md5函数
             md.update(str.getBytes());
             return new BigInteger(1, md.digest()).toString(16);
         } catch (Exception e) {
