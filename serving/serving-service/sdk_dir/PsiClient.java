0a354ab60d0613cfd16fb69b9e0a344fe7060a4d
winter.zou <winter.zou@welab-inc.com>
2022-12-14 17:55:37 +0800

    update sdk

--- serving/serving-service/sdk_dir/PsiClient.java
+++ serving/serving-service/sdk_dir/PsiClient.java
@@ -15,11 +15,28 @@
  */
 
 package com.welab.wefe.mpc.psi.sdk;
+/*
+ * Copyright 2021 Tianmian Tech. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.welab.wefe.mpc.config.CommunicationConfig;
 
@@ -31,31 +48,25 @@ public class PsiClient {
     // 公钥
     private static final String 测试客户1_publicKey = "***";
     // 客户code
-    private static final String 测试客户1_code = "***";
+    private static final String 测试客户1_code = "****";
     // Serving服务地址
-    private static final String serverUrl = "https://xxx.xxx.com/xxx/"; // TODO
+    private static final String serverUrl = "http://xxxxxx/serving-service/"; // TODO
     // Service Api name
-    private static final String apiName = "api/xxx/xxx"; // TODO
+    private static final String apiName = "api/user/query"; // TODO
 
-    private static List<String> clientDataset;
+    private static Map<String, String> clientDatasetMap; // 哈希数据，原数据
 
-    /**
-     * 初始化客户端数据
-     */
     private static void initClientDataset() {
-        clientDataset = new ArrayList<>();
-        int total = 10000;
-        while ((total--) > 0) {
-            clientDataset.add(getMD5String("" + total * 10));
-        }
+        clientDatasetMap = new LinkedHashMap<>();
+        // TODO
     }
 
     public static void main(String[] args) throws Exception {
         initClientDataset();
-
         long start = System.currentTimeMillis();
         // params
-        Psi psi = PsiFactory.generatePsi("dh"); // dh or ecdh
+        Psi psi = PsiFactory.generatePsi();
+        psi.setClientDatasetMap(clientDatasetMap);
         CommunicationConfig config = new CommunicationConfig();
         config.setNeedSign(true); // 是否需要签名
         config.setSignPrivateKey(测试客户1_privateKey);// 私钥
@@ -64,13 +75,11 @@ public class PsiClient {
         config.setServerUrl(serverUrl);
         config.setApiName(apiName);
         // 如果是续跑 需要带上下面两个参数
-        // config.setRequestId("");
-        // config.setContinue(true);
-        List<String> result = psi.query(config, clientDataset);
-        // 如果 clientId = result 说明有结果
-        System.out.println(clientDataset.size());
-        System.out.println(result.size());
-        System.out.println("duration = " + (System.currentTimeMillis() - start));
+//        config.setRequestId("xxxxxxx");
+//        config.setContinue(true);
+        List<String> result = psi.query(config, new ArrayList<>(clientDatasetMap.keySet()));
+        System.out.println("client size = " + clientDatasetMap.size() + ", result size = " + result.size()
+                + ", duration = " + (System.currentTimeMillis() - start));
     }
 
     public static String getMD5String(String str) {
