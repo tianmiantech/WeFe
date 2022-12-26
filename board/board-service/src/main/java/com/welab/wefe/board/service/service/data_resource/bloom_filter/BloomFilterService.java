3562601598e8a35bc8ec1524dd639446d5cad7c6
winter.zou <winter.zou@welab-inc.com>
2022-12-26 14:42:41 +0800

    update

--- board/board-service/src/main/java/com/welab/wefe/board/service/service/data_resource/bloom_filter/BloomFilterService.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/service/data_resource/bloom_filter/BloomFilterService.java
@@ -152,6 +152,7 @@ public class BloomFilterService extends DataResourceService {
         repo.deleteById(model.getId());
 
         // delete bloom_filter from folder
+        // 查看代码发现bloomfilterStorageService的save相关方法都没被调用过，因此不会保存到ck里面去，所以不需要调用这个方法
         bloomfilterStorageService.deleteBloomfilter(model.getId());
 
         // Notify the union to do not public the bloom_filter
