b47c890f224d2d70d872d260db43c85f10aa5889
winter.zou <winter.zou@welab-inc.com>
2023-01-03 13:59:31 +0800

    修复排序设置没有生效的问题

--- board/board-service/src/main/java/com/welab/wefe/board/service/service/ServingService.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/service/ServingService.java
@@ -387,7 +387,6 @@ public class ServingService extends AbstractService {
                                 getJSONObject(0).
                                 getJSONObject("ext_json").
                                 getString("serving_base_url"));
-                        LOG.info("fillPublicKey member = " + json.toJSONString());
                     } catch (StatusCodeWithException e) {
                         super.log(e);
                     }
