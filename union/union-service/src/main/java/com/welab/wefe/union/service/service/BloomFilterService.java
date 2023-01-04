988f1ea77dc272933e94c52a53ca821506128a53
aaron.li <aaron.li@welab-inc.com>
2023-01-04 15:35:27 +0800

    修复ModelMapper的属性同名但类型不同时映射不上问题

--- union/union-service/src/main/java/com/welab/wefe/union/service/service/BloomFilterService.java
+++ union/union-service/src/main/java/com/welab/wefe/union/service/service/BloomFilterService.java
@@ -21,9 +21,9 @@ import com.welab.wefe.common.data.mongodb.entity.union.DataResource;
 import com.welab.wefe.common.data.mongodb.repo.BloomFilterMongoReop;
 import com.welab.wefe.common.exception.StatusCodeWithException;
 import com.welab.wefe.common.util.DateUtil;
-import com.welab.wefe.common.web.util.ModelMapper;
 import com.welab.wefe.union.service.api.dataresource.bloomfilter.PutApi;
 import com.welab.wefe.union.service.service.contract.BloomFilterContractService;
+import com.welab.wefe.union.service.util.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
