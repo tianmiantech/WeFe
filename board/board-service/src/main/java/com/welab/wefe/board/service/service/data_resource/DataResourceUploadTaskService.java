0c992b87f65fffa38c702cb0b02bd557e6b33ff6
zane.luo <zane.luo@welab-inc.com>
2023-02-08 14:44:01 +0800

    优化资源上传预估耗时计算方法

--- board/board-service/src/main/java/com/welab/wefe/board/service/service/data_resource/DataResourceUploadTaskService.java
+++ board/board-service/src/main/java/com/welab/wefe/board/service/service/data_resource/DataResourceUploadTaskService.java
@@ -138,7 +138,7 @@ public class DataResourceUploadTaskService extends AbstractService {
             long estimateTime = 0;
             if (progress < 100) {
                 long spend = System.currentTimeMillis() - task.getCreatedTime().getTime();
-                estimateTime = spend / progress * (100 - progress);
+                estimateTime = (totalDataRowCount - completedDataCount) / completedDataCount * spend;
             }
 
             task.setTotalDataCount(totalDataRowCount);
