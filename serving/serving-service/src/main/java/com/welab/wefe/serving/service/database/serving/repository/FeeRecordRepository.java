dafab69196687b4de06c144855559b2418d72430
aaron.li <aaron.li@welab-inc.com>
2023-11-08 15:39:32 +0800

    serving: 修复group by的bug

--- serving/serving-service/src/main/java/com/welab/wefe/serving/service/database/serving/repository/FeeRecordRepository.java
+++ serving/serving-service/src/main/java/com/welab/wefe/serving/service/database/serving/repository/FeeRecordRepository.java
@@ -44,7 +44,7 @@ public interface FeeRecordRepository extends BaseRepository<FeeDetailOutputModel
      * @return
      */
     @Query(value = "select concat(fd.service_id, fd.client_id,fd.fee_config_id,max(fd.created_time)) as id, max(fd.service_name) as service_name,fd.service_id as service_id, max(fd.client_name) as client_name, " +
-            "fd.client_id as client_id ,max(fd.service_type) as service_type ,max(fd.unit_price) as unit_price,DATE_FORMAT(fd.created_time ,:query_type) as query_date , " +
+            "fd.client_id as client_id ,max(fd.service_type) as service_type ,max(fd.unit_price) as unit_price,DATE_FORMAT(max(fd.created_time) ,:query_type) as query_date , " +
             "max(fd.pay_type) as pay_type, sum(fd.total_request_times) as total_request_times, sum(fd.total_fee) as total_fee, fd.fee_config_id " +
             "from fee_detail fd  " +
             "where if(:service_name !='', fd.service_name like concat('%',:service_name,'%'), 1=1) " +
